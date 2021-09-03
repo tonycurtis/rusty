2da276296751c8f71390984a415b7c937398f226
Tony Curtis <anthony.curtis@stonybrook.edu>
2021-09-03 14:50:15 -0400

    Add sum2n example

---
+++ sum2n/src/main.rs
@@ -0,0 +1,29 @@
+use shmem;
+use std::mem;
+
+fn main() {
+    shmem::init();
+
+    let counter = shmem::malloc(1 * mem::size_of::<i32>()) as *mut i32;
+    unsafe {
+	*counter = 0;
+    }
+    
+    let me = shmem::my_pe();
+
+    shmem::int_atomic_add(counter, me + 1, 0);
+
+    shmem::barrier_all();
+
+    if me == 0 {
+	let n = shmem::n_pes();
+
+	unsafe {
+	    println!("Sum from 1 to {} = {}", n, *counter);
+	}
+    }
+
+    shmem::free(counter as shmem::SymmMemAddr);
+
+    shmem::finalize();
+}
