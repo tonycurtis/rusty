b3e5d1a5e8cfb366b07ef027a53583bd2395480d
Tony Curtis <anthony.curtis@stonybrook.edu>
2021-10-21 11:17:56 -0400

    code cleanup

--- rotput/src/main.rs
+++ rotput/src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     shmem::init();
 
-    // let mut r: shmem::RDMA;
-
     let me = shmem::my_pe();
     let n = shmem::n_pes();
 
@@ -18,9 +16,6 @@ fn main() {
     let dest = shmem::malloc(1 * mem::size_of::<i32>()) as *mut i32;
 
     shmem::int_p(dest, nextpe, nextpe);
-
-    // let xx = shmem::RdmaOp { dest: dest, src: nextpe, pe: nextpe };
-    // shmem::p(xx);
 
     shmem::barrier_all();
 
