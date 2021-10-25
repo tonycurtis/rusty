2f698d5916061f1a47aadfc4c83a9ccc2131494d
Tony Curtis <anthony.curtis@stonybrook.edu>
2021-10-25 11:02:48 -0400

    barrier needed

--- sum2n/src/main.rs
+++ sum2n/src/main.rs
@@ -6,9 +6,14 @@ fn main() {
 
     let counter = shmem::malloc(1 * mem::size_of::<i32>()) as *mut i32;
     unsafe {
-	*counter = 0;
+	    *counter = 0;
     }
-    
+
+    shmem::barrier_all();
+
+    // counter is now 0 everywhere
+
+    // add me+1 to PE 0
     let me = shmem::my_pe();
 
     shmem::int_atomic_add(counter, me + 1, 0);
@@ -16,11 +21,11 @@ fn main() {
     shmem::barrier_all();
 
     if me == 0 {
-	let n = shmem::n_pes();
+	    let n = shmem::n_pes();
 
-	unsafe {
-	    println!("Sum from 1 to {} = {}", n, *counter);
-	}
+	    unsafe {
+	        println!("Sum from 1 to {} = {}", n, *counter);
+	    }
     }
 
     shmem::free(counter as shmem::SymmMemAddr);
