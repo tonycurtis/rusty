dc0e0f61212213c9bc9ee7fc7786258f099f5b31
Tony Curtis <anthony.curtis@stonybrook.edu>
2021-10-27 13:20:05 -0400

    untabify

--- sum2n/src/main.rs
+++ sum2n/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let counter = shmem::malloc(1 * mem::size_of::<i32>()) as *mut i32;
     unsafe {
-	    *counter = 0;
+        *counter = 0;
     }
 
     shmem::barrier_all();
@@ -21,11 +21,11 @@ fn main() {
     shmem::barrier_all();
 
     if me == 0 {
-	    let n = shmem::n_pes();
+        let n = shmem::n_pes();
 
-	    unsafe {
-	        println!("Sum from 1 to {} = {}", n, *counter);
-	    }
+        unsafe {
+            println!("Sum from 1 to {} = {}", n, *counter);
+        }
     }
 
     shmem::free(counter as shmem::SymmMemAddr);
